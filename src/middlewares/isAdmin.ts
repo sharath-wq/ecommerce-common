8848ec7972b3779072595158b5b83e5b6a733332
Sharath-wq <chandranpsharath@gmail.com>
2024-01-23 16:29:33 +0530

    Updates

---
+++ src/middlewares/isAdmin.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from '../errors/not-authorized-error';
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.currentUser?.isAdmin) {
+        throw new NotAuthorizedError();
+    }
+
+    next();
+};
