899d74d8afaa6e0fa4f8c85a9e98c6fd4022d87e
Sharath-wq <chandranpsharath@gmail.com>
2024-01-24 12:41:30 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,3 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
-export * from './middlewares/isAdmin';
