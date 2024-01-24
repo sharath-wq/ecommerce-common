c97b275c0d7c95fd3cfebf4fd1d21463809bcda5
Sharath-wq <chandranpsharath@gmail.com>
2024-01-24 23:35:02 +0530

    Updates

---
+++ src/events/cart-created-event.ts
@@ -0,0 +1,8 @@
+import { Subjects } from './subjects';
+
+export interface CartCreatedEvent {
+    subject: Subjects.CartCreated;
+    data: {
+        id: string;
+    };
+}
--- src/events/subjects.ts
+++ src/events/subjects.ts
@@ -3,4 +3,6 @@ export enum Subjects {
     ProductUpdated = 'product:updated',
 
     UserCreated = 'user:created',
+
+    CartCreated = 'cart:created',
 }
